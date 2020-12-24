f9b16816eebdf40188d164b8e2a1045ec0399fc3
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-12-24 22:37:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //import using require
 const express = require('express');
 const mongoose = require('mongoose');
-const Blog = require('./models/user');
+const user = require('./models/user');
 
 const dbURI = "mongodb+srv://netninja:TeroTero@netninja.krkpa.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 
 //express app (instanciate)
@@ -12,7 +16,7 @@ const app = express();
 app.use(express.static('public'));
 
 // listen for requests
-app.listen(3000);
+//app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
@@ -33,6 +37,27 @@ app.get('/', (req, res) => {
 app.post('/home', (req, res) => {
     res.render('home', { email: req.body.email , password:req.body.password });
     console.log(req.body);
+    //
+    // user.find({ email: 'akash.adhikary@hotmail.com'}, function (err, docs) { 
+    //   if (err){ 
+    //       console.log(err); 
+    //   } 
+    //   else{ 
+    //       console.log("First function call : ", docs); 
+    //   } 
+    // }); 
+    // 
+
+    user.find({ email: req.body.email})
+    .then(result => {
+      // res.render('details', { blog: result, title: 'Blog Details' });
+      console.log(2);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
   });
 
 // redirects
