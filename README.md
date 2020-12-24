868a1562c2e8cc0edb626a7040b5e5689826d872
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-12-24 12:25:10 +0530

    host a static website using node.js
    Basic express functions and static files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node-tutorial
  Node.js Crash Course By Akash Adhikary
+
+Basic tutorial on nodejs for making a server using express library
+
+#initialize npm - npm init
+#install nodemon- npm install nodemon
+#install express- npm install express
---
+++ app.js
@@ -0,0 +1,30 @@
+//import using require
+const express = require('express');
+
+//express app (instanciate)
+const app = express();
+//satic files directory
+app.use(express.static('public'));
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/login.html', { root: __dirname });
+});
+
+app.get('/home', (req, res) => {
+  // res.send('<p>about page</p>');
+  res.sendFile('./views/home.html', { root: __dirname });
+});
+
+// redirects
+app.get('/profile', (req, res) => {
+  res.redirect('/home');
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
