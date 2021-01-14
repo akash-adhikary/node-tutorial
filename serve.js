7f27580145a9702cbac728896d603a3de291c434
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2021-01-14 23:06:30 +0530

    js node connect

---
+++ serve.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes')
+const dbURI = "mongodb+srv://netninja:TeroTero@netninja.krkpa.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+//express app (instanciate)
+const app = express();
+app.set('trust proxy', 1) // trust first proxy
+//satic files directory
+app.use(express.static('public'));
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/log', function(req, res) {
+    console.log("hi");
+    console.log(req.body.chat);
+});
